1cb51e593eb238406fdba6813b15db0ae33d64ca
jiyeon22 <kkll4106@naver.com>
2021-09-26 11:19:18 +0900

    데이터읽기

--- lite/examples/posenet/android/app/src/main/java/org/tensorflow/lite/examples/posenet/ScoreActivity.kt
+++ lite/examples/posenet/android/app/src/main/java/org/tensorflow/lite/examples/posenet/ScoreActivity.kt
@@ -5,13 +5,16 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_logout.*
 import kotlinx.android.synthetic.main.activity_logout.btnSignOut
 import kotlinx.android.synthetic.main.activity_score.*
+import kotlinx.android.synthetic.main.tfe_pn_activity_posenet.*
 import org.tensorflow.lite.examples.posenet.Extensions.toast
+import java.io.Serializable
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -76,14 +79,27 @@ class ScoreActivity : AppCompatActivity() {
 
 
 
-        data class Score(var count_good: String? = null,var count_normal: String? = null,var count_bad: String? = null,var score: String? = null)
+        data class Score(
+            var count_good: String? = null,
+            var count_normal: String? = null,
+            var count_bad: String? = null,
+            var score: String? = null) :Serializable
+
+
+
 
         fun writeNewScore(count_good: String,count_normal: String,count_bad: String,score: String) {
-            val score = Score(count_good,count_normal,count_bad,score)
-            database= Firebase.database.reference
-            FirebaseUtils.firebaseAuth.uid?.let { database.child("scores").child(it).child(Date).child(ClickState).setValue(score) }
 
+            val score = Score(count_good, count_normal, count_bad, score)
+            database = Firebase.database.reference
+            FirebaseUtils.firebaseAuth.uid?.let {
+                database.child("scores").child(it).child(Date).child(ClickState).setValue(score)
+            }
         }
+
+
+
+
 
         btnSignOut.setOnClickListener({
             FirebaseUtils.firebaseAuth.signOut()
@@ -97,7 +113,36 @@ class ScoreActivity : AppCompatActivity() {
             val intent = Intent(this, MainloginActivity::class.java)
             startActivity(intent)
             writeNewScore(count_good,count_normal,count_bad,score)
+
+
+            //데이터 읽기
+            val scoreReference = FirebaseDatabase.getInstance().getReference("Score")
+
+            val postListener = object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    // Get Post object and use the values to update the UI
+                    val post = dataSnapshot.getValue<Score>()
+                    // ...
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    // Getting Post failed, log a message
+                    Log.w("TAG", "loadPost:onCancelled", databaseError.toException())
+                }
+            }
+            scoreReference.addValueEventListener(postListener)
+
+            database.child("Score").child(Date).get().addOnSuccessListener {
+                Log.i("firebase읽기", "Got value ${it.value}")
+            }.addOnFailureListener{
+                Log.e("firebase읽기", "Error getting data", it)
+            }
         }
+
+
+
     }
+
+
 }
 
